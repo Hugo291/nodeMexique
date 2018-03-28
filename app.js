01d743c45d8f96a7dc513059362466efb9857350
Hugo291 <33847075+Hugo291@users.noreply.github.com>
2018-03-28 05:43:41 +0200

    add upload sys and convert pdf to image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+fileUpload = require('express-fileupload');
 
 //Routes of all files
 var AllRouter = require('./routes/routes');
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload());
 //Routes of all files in route folder
 app.use('/', AllRouter);
 //app.use('/users', usersRouter);
--- routes/translate/selectionExtract.js
+++ routes/translate/selectionExtract.js
@@ -1,54 +1,79 @@
-var formidable = require('formidable');
+//random integer
+var randomInt = require('random-int');
+//pdf to jpg
+var pdf2img = require('pdf2img');
+//date (now)
+var date = require('date-and-time');
+//file system
 var fs = require('fs');
-var path = require('path');
+//folder not exist
+var mkdirp = require('mkdirp');
+
 
 module.exports = function(req,res){
 
-    //tramission of form
-    var form = new formidable.IncomingForm();
-    //The upload dir
-    form.uploadDir = __dirname+'\\uploads\\';
-    //If the the keep the extension in name
-    form.keepExtensions = true;
-    //if allow mutliple files upload
-    form.multiples = false;
-    //Begin of upload file
-    form.on('fileBegin', function(name, file) {
+    //If request has  no files stop the proccess
+    if (!req.files)
+        return res.send('No files were uploaded.');
 
-        //If format is png
-        if(path.extname(__dirname+'\\upload\\'+file.name).toLowerCase() === ".png"){
-            //Rename of file
-            file.path = form.uploadDir+file.name;
-            //console.log("Path : "+__dirname+'\\upload\\'+file.name);
-            console.log("FileName  : "+file.name);
+    //The format of file
+    var format = 'jpg';
+    //The date and hour of noww
+    var now = new Date();
+    //The name of file generate YYYYMMDDHHmmss + RandomInt
+    var filename = date.format(now, 'YYYYMMDDHHmmss' )+randomInt(10,10000);
+    //Set the var file
+    var pdfFileUpload = req.files.documentPdf;
 
-            //In end of upload
-            form.on('end', function() {
-                console.log('upload is finish');
-            });
+    //Ckeck if the type is pdf
+    if(pdfFileUpload.mimetype !== 'application/pdf'){
+        return res.send('Type incorrect');
+    }
 
-            var spawn = require('child_process').spawn,
-            py = spawn('python', ['AnalyseImage.py' , file.path]),
-            dataString = '';
+    //Destionation of final pdf (The big file)
+    var destPdfFile = 'uploads\\pdf\\'+filename+'.pdf';
 
-            py.stdout.on('data', function(data){
-                dataString += data.toString();
-            });
-
-            /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
-            py.stdout.on('end', function(){
-                console.log('Sum of numbers=',dataString);
-                res.render("translate/selectionExtract.twig",{'data':dataString} );
-            });
-        }else{
-            res.redirect('upload?error=fileformat');
+    pdfFileUpload.mv(destPdfFile, function(err) {
+        //if error during the mv
+        if (err) {
+            console.log(err);
+            return res.send('Erreur lors du mouvement du fichier');
         }
-    });
 
-    form.on('part',function () {
+        console.log('the file is upload');
+        //pdf options
+        pdf2img.setOptions({
+            type: 'jpg',
+            size: 1024,
+            density: 200,
+            outputdir: 'uploads/'+format+'/'+filename,
+            outputname: null,
+            page: null
+        });
 
-    });
-    //Transmission of req to file
-    form.parse(req);
+        //Create a folder where images are stored
+        mkdirp('uploads/'+format+'/'+filename, function(err) {
+
+            if(err) {
+                console.log(err);
+                return res.send('Error creation of ');
+            }
+
+            console.log('Convertion of pdf to jpg');
+            //Convert destpdffile to images
+            pdf2img.convert(destPdfFile, function(err, info) {
+                if (err) {
+                    console.log(err);
+                    return res.send('Erreur lors du mouvement du fichier');
+                }else
+                    //OCR is here
+                    console.log(info);
+                res.render('translate/selectionExtract',{'data':info});
+            });//END pdf2img.convert
+
+        });//END mkdirp
+
+    });//End pdfFileUpload
 
 };
+
